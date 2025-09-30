f751a0e5a9ce94bbc9f067c2c6550cbbcf4f1aab
David L Gibbs <gibbsdavidl@gmail.com>
2025-09-30 16:11:17 -0700

    fixed cli example script

--- scripts/run_cli.sh
+++ scripts/run_cli.sh
@@ -1,9 +1,18 @@
 tbccsi \
-  --sample_id   SH_8_Pre \
-  --input_slide "slides/SH-8 Pre/SH-8 Pre.svs" \
-  --tile_file   results/SH_8_Pre/common_tiling.csv \
-  --output_dir  results/SH_8_Pre/ \
-  --models      models/mixed_224_c/model_dir models/mixed_224_i/model_dir models/mixed_224_s/model_dir models/mixed_imm_m_224/model_dir models/mixed_imm_t_224/model_dir\
-  --prefixes    cancer immune stroma macs tcells \
-  --batch_size  264 \
-  --use_segmented_tiles # overrides writing, reads from output_dir/segmented_tiles
+  --sample-id   SH_8_Pre \
+  --input-slide "slides/SH-8 Pre/SH-8 Pre.svs" \
+  --tile-file   results/SH_8_Pre/common_tiling.csv \
+  --output-dir  results/SH_8_Pre/ \
+  --models      models/mixed_224_c/model_dir \
+  --models      models/mixed_224_i/model_dir \
+  --models      models/mixed_224_s/model_dir \
+  --models      models/mixed_imm_m_224/model_dir \
+  --models      models/mixed_imm_t_224/model_dir \
+  --prefixes    cancer \
+  --prefixes    immune \
+  --prefixes    stroma \
+  --prefixes    macs \
+  --prefixes    tcells \
+  --batch-size  264 \
+  --save-h-and-e-tiles \
+  --save-segmented-tiles
